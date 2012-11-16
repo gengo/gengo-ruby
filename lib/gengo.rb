e7d92ee1c40e8a4c03f00d7b4eb967d3c17efeb8
Shawn Smith <shawnpsmith@gmail.com>
2012-11-16 17:42:12 +0900

    fix SANDBOX_API_HOST

--- lib/gengo.rb
+++ lib/gengo.rb
@@ -8,7 +8,7 @@ module Gengo
 		# API url endpoints; replace the version at function call time to
 		# allow for function-by-function differences in versioning.
 		API_HOST = 'api.gengo.com'
-		SANDBOX_API_HOST = 'api.sandbox.gengo.com'
+		SANDBOX_API_HOST = 'api.sandbox.mygengo.com'
 
 		# Pretty self explanatory.
 		VERSION = '1.16'
