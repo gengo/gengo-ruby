2791566943abb9c3f10368d04930803829c55909
ksk-uchi <ksk.uchi+sl6.6base@gmail.com>
2015-08-17 12:48:14 +0900

    hash syntax follows new syntax

--- examples/getTranslationOrderComments.rb
+++ examples/getTranslationOrderComments.rb
@@ -3,10 +3,10 @@ require 'gengo'
 
 # Get all comments on an order.
 
-gengo = Gengo::API.new({
-  :public_key => 'your_public_key',
-  :private_key => 'your_private_key',
-  :sandbox => true,
-})
+gengo = Gengo::API.new(
+  public_key: 'your_public_key',
+  private_key: 'your_private_key',
+  sandbox: true,
+)
 
-puts gengo.getTranslationOrderComments({:id => 42})
+puts gengo.getTranslationOrderComments(id: 42)
--- examples/postTranslationOrderComment.rb
+++ examples/postTranslationOrderComment.rb
@@ -1,11 +1,11 @@
 # encoding: UTF-8
 require 'gengo'
 
-gengo = Gengo::API.new({
-  :public_key => 'your_public_key',
-  :private_key => 'your_private_key',
-  :sandbox => true,
-})
+gengo = Gengo::API.new(
+  public_key: 'your_public_key',
+  private_key: 'your_private_key',
+  sandbox: true,
+)
 
 # Posts a comment to order #42.
-puts gengo.postTranslationOrderComment({:id => 42, :body => "I love lamp!"})
+puts gengo.postTranslationOrderComment(id: 1702357, body: "I love lamp!")
